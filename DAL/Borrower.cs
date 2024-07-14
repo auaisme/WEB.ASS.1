d977e2a153d16a2134f4a355dcec7c07d029fdea
auaisme <121776575+auaisme@users.noreply.github.com>
2024-07-14 18:43:55 +0500

    Add files via upload
    Commit 1/2. Code isn't the cleanest since I didn't want to re-write the entire solution. Anyway, moved from file system to db system.

---
+++ DAL/Borrower.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Borrower
+    {
+        private int borrowerId;
+        private string name, email;
+
+        public Borrower(int borrowerId, string name, string email)
+        {
+            this.borrowerId = borrowerId;
+
+            name = name.Trim();
+            name = name.ToLower();
+            this.name = name;
+
+            email = email.Trim();
+            email = email.ToLower();
+            this.email = email;
+        }
+
+        public int getBorrowerId() { return borrowerId; }
+
+        public string getName() { return name; }
+
+        public string getEmail() { return email; }
+
+        public void print()
+        {
+            Console.WriteLine($"ID: {borrowerId}, Name: {name}, Email: {email}");
+        }
+    }
+}
---
+++ DAL/Transaction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Transaction
+    {
+        private int transactionId, bookId, borrowerId;
+        private bool isBorrowed;
+        private DateTime date;
+
+        public Transaction(int transactionId, int bookId, int borrowerId, bool isBorrowed, DateTime date)
+        {
+            this.transactionId = transactionId;
+            this.bookId = bookId;
+            this.borrowerId = borrowerId;
+            this.isBorrowed = isBorrowed;
+            this.date = date;
+        }
+
+        public int getTransactionId() { return transactionId; }
+
+        public int getBookId() { return bookId; }
+
+        public int getBorrowerId() { return borrowerId; }
+
+        public bool getIsBorrowed() { return isBorrowed; }
+
+        public DateTime getDate() { return date; }
+
+        public void print()
+        {
+            Console.WriteLine($"ID: {transactionId}, Book ID: {bookId}, Borrower ID: {borrowerId}, Borrowed: {isBorrowed}, Date: {date}");
+        }
+    }
+}
